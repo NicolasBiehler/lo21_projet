3069cc147f393dba1ec481e704ca3cab9d7c02a2
JiB <jeanbaptiste.arzounian@gmail.com>
2012-06-01 15:12:16 +0200

    mise a jour interface

--- ProjetCalculatrice/mainwindow.h
+++ ProjetCalculatrice/mainwindow.h
@@ -14,6 +14,18 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+public slots:
+    void num0Pressed();
+    void num1Pressed();
+    void num2Pressed();
+    void num3Pressed();
+    void num4Pressed();
+    void num5Pressed();
+    void num6Pressed();
+    void num7Pressed();
+    void num8Pressed();
+    void num9Pressed();
     
 private:
     Ui::MainWindow *ui;
